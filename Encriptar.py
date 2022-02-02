1a7a68ec67bcac05238aae4ba5079ba3c6ad8f2f
beatrizmotta <62359366+beatrizmotta@users.noreply.github.com>
2022-02-02 10:30:24 -0300

    Update Encriptar.py

--- Encriptar.py
+++ Encriptar.py
@@ -1,12 +1,43 @@
 def encriptar(escolhaInicial):
 
-    mensagem = input("Digite o texto que você quer encriptar: ")
-    cp = int(input("Digite a sua chave pública: ")
-    mensagemEncriptada = [ord(x) - 95 for x in mensagem]
+    # Criando uma lista com alguns caracteres da tabela ASCII (alfabeto + alguns símbolos)
+    alphabet = list()
+    for i in range(47, 120):
+        alphabet.append(chr(i))
 
-    novaMsgmEncrip = str(mensagemEncriptada)
     
-    arquivo = open("arquivo.txt", "a")
-    arquivo.writelines(novaMsgmEncrip + '\n')
+    # Pede o texto para o usuário
+    mensagem = input("Digite o texto que você quer encriptar: ")
+    chave = int(input("Digite a chave pública: "))
+
+    # Codifica a mensagem
+    def codify(char, chave):
+        index = 0
+        codified = list()
+
+        # Vai letra por letra da mensagem e vê se ela existe na lista que criamos anteriormente
+        for i in range(0, len(char)):
+            for k in range(0, len(alphabet)):
+                # Caso não exista, insere o caractere como ele está 
+                if (char[i] not in alphabet):
+                    codified.append(char[i])
+                    break
+                else:
+                    # Caso exista, insere o caractere transposto x caracteres para a frente 
+                    if (char[i] == alphabet[k]):
+                        index = k
+                        index = (index + chave) % 75
+                        codified.append(alphabet[index])
+                        
+        return codified;
     
-    print("Sua mensagem encriptada é:", novaMsgmEncrip)
+
+
+    mensagemEncriptada = ''.join(codify(mensagem, int(chave)))
+    print(mensagemEncriptada)
+
+    arquivo = open("mensagem_encriptada.txt", "w")
+    arquivo.writelines(mensagemEncriptada)
+        
+    print("Sua mensagem encriptada é:", mensagemEncriptada)
+    
