a995d9b93bc35b1737fc1cab0a76bbba3d5d650a
beatrizmotta <62359366+beatrizmotta@users.noreply.github.com>
2022-02-02 12:00:04 -0300

    Update Encriptar.py

--- Encriptar.py
+++ Encriptar.py
@@ -2,16 +2,18 @@ def encriptar(escolhaInicial):
 
     # Criando uma lista com alguns caracteres da tabela ASCII (alfabeto + alguns símbolos)
     alphabet = list()
-    for i in range(47, 120):
+    for i in range(65, 91):
         alphabet.append(chr(i))
+    alphabet.append(' ')
 
     
     # Pede o texto para o usuário
     mensagem = input("Digite o texto que você quer encriptar: ")
+    mensagem = mensagem.upper()
     chave = int(input("Digite a chave pública: "))
 
     # Codifica a mensagem
-    def codify(char, chave):
+    def codify(char):
         index = 0
         codified = list()
 
@@ -26,15 +28,17 @@ def encriptar(escolhaInicial):
                     # Caso exista, insere o caractere transposto x caracteres para a frente 
                     if (char[i] == alphabet[k]):
                         index = k
-                        index = (index + chave) % 75
-                        codified.append(alphabet[index])
+                        index = (index + 2) % 29
+                        codified.append(index)
                         
         return codified;
     
-
-
-    mensagemEncriptada = ''.join(codify(mensagem, int(chave)))
-    print(mensagemEncriptada)
+    msg_to_numbers = codify(mensagem)
+    mensagemEncriptada = list()
+    for i in msg_to_numbers:
+        mensagemEncriptada.append(str(i))
+    
+    mensagemEncriptada = ''.join(mensagemEncriptada)
 
     arquivo = open("mensagem_encriptada.txt", "w")
     arquivo.writelines(mensagemEncriptada)
